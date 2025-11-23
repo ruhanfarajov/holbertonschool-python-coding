a082e8b092dc44102f3eb3ff72b14791f2578443
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-23 14:10:47 -0500

    l;kad;lksa

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """Represents a Square"""
+    def __init__(self, size):
+        """Initialize a new square with validation"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be positive")
+        self.__size = size
