21301fd232dd37115a057a45798493a0524ca445
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-23 14:13:06 -0500

    kjkdfsdk

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Represents a Square"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new square with validation"""
 
         if not isinstance(size, int):
