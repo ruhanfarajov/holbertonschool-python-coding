dd320ff84422e471f6cd46f69df117294ef68ff8
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-23 13:59:19 -0500

    klsdka

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines a square class"""
+
 
 class Square:
+    """Represents a Square"""
     def __init__(self, size):
+        """Initialize a new square with a private size."""
         self.__size = size
