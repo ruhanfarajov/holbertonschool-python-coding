fe27d576f8fb468283f29adc9811fe21faacf588
ruhanfarajov <ruhanafarajov@gmail.com>
2025-11-23 14:39:28 -0500

    ksdcxvms

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """Represents a Square"""
+    def __init__(self, size=0):
+        """Initialize a new square with validation"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the areas of the square"""
+        return self.__size**2
+    
+    def size(self):
+        """Retrieves the size of the object"""
+        return self.__size
+
+    def set_size(self, name=None):
+        """allows setiing size for the self__size variable"""
+        self.__size = name
